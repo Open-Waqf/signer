a847881380f71f8a31c0d1d1bec80081cfa47d98
Open Waqf Contributor <contact@open-waqf.org>
2026-02-03 13:51:33 +0100

    Add text and more

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,15 @@ declare module '*?url' {
     const content: string;
     export default content;
 }
+
+declare module 'virtual:pwa-register' {
+    export interface RegisterSWOptions {
+        immediate?: boolean
+        onNeedRefresh?: () => void
+        onOfflineReady?: () => void
+        onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void
+        onRegisterError?: (error: any) => void
+    }
+
+    export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>
+}
